67eced193e68648fa0bb7b16ed1888ef88e817be
KieMerr17 <kieran.merrett@hotmail.com>
2023-12-31 14:21:34 +0000

    add average and variability images to page two

--- app_pages/page_two_data_visualisation.py
+++ app_pages/page_two_data_visualisation.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 def page_two_content():
     st.write("### Awareness Visualisation")
@@ -6,3 +12,18 @@ def page_two_content():
         "To fulfill the first business requirement, we have developed a visualisation technique capable of distinguishing "
         "between a drivers eyes which are aware and open and that of eyes which are closed."
     )
+
+    version = 'v1'
+    if st.checkbox("Difference between average images from each set and the variability across those images"):
+
+        avg_eyes_open = plt.imread(f"outputs/{version}/avg_var_eyes_open.png")
+        avg_eyes_closed = plt.imread(f"outputs/{version}/avg_var_eyes_closed.png")
+
+        st.warning(
+            "*The average and variability images didn't show obvious "
+            "patterns where it was clear to differentiate one to another. "
+            "However, eyes_closed images did show more white in the centre of the images")
+        
+        st.image(avg_eyes_open, caption="Eyes Open: Average and Variability")
+        st.image(avg_eyes_closed, caption="Eyes Closed: Average and Variability")
+        st.write("---")
