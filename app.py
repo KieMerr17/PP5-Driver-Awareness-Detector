d98a9f5e9e4fa6bf7f0f9f3afe32fe32cde8b25f
KieMerr17 <kieran.merrett@hotmail.com>
2023-12-31 14:14:33 +0000

    add app/page two_data_visualisation, brief content and connect to multipage

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from app_pages.multipage import MultiPage
 
 # Import from the functions from each page to be shown on the dashboard
 from app_pages.page_one_summary import page_one_content
+from app_pages.page_two_data_visualisation import page_two_content
+
 
 # App name
 app = MultiPage(app_name= "Driver Awareness Detector")
 
 # page names and functions to be imported
 app.add_page("Project Summary", page_one_content)
+app.add_page("Data Visualisation", page_two_content)
+
 
 
 app.run()
---
+++ app_pages/page_two_data_visualisation.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def page_two_content():
+    st.write("### Awareness Visualisation")
+    st.info(
+        "To fulfill the first business requirement, we have developed a visualisation technique capable of distinguishing "
+        "between a drivers eyes which are aware and open and that of eyes which are closed."
+    )
