d0867a9884249a81b02747113c758c86eae91238
KieMerr17 <kieran.merrett@hotmail.com>
2023-12-31 14:51:16 +0000

    add app/page three_awareness_detector, brief content and connect to multipage

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from app_pages.multipage import MultiPage
 # Import from the functions from each page to be shown on the dashboard
 from app_pages.page_one_summary import page_one_content
 from app_pages.page_two_data_visualisation import page_two_content
+from app_pages.page_three_awareness_detector import page_three_content
 
 
 # App name
@@ -12,6 +13,8 @@ app = MultiPage(app_name= "Driver Awareness Detector")
 # page names and functions to be imported
 app.add_page("Project Summary", page_one_content)
 app.add_page("Data Visualisation", page_two_content)
+app.add_page("Awareness Detector", page_three_content)
+
 
 
 
---
+++ app_pages/page_three_awareness_detector.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def page_three_content():
+
+    st.info(
+        "To acheive the business requirement, you can upload a picture of a drivers eyes and detect their awareness. "
+        "It will identify if the image displays a drivers eyes as 'eyes open' or 'eyes closed'."
+    )
