77ba97db71a25108256acc40f27a64ebdacb80bd
KieMerr17 <kieran.merrett@hotmail.com>
2023-12-31 16:00:21 +0000

    add link to model images and image uploader to page_three

--- app_pages/page_three_awareness_detector.py
+++ app_pages/page_three_awareness_detector.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
+import pandas as pd
+
 
 def page_three_content():
 
@@ -6,3 +10,20 @@ def page_three_content():
         "To acheive the business requirement, you can upload a picture of a drivers eyes and detect their awareness. "
         "It will identify if the image displays a drivers eyes as 'eyes open' or 'eyes closed'."
     )
+
+    st.write(
+        "Images used to train the model can be found "
+        "[here](https://www.kaggle.com/datasets/kutaykutlu/drowsiness-detection)."
+    )
+
+    st.write(
+        "---"
+    )
+
+    st.write(
+        "**Upload your image below, you may select multiple images**"
+    )
+
+    image_buffer = st.file_uploader('', type='jpeg', accept_multiple_files=True)
+
+    
