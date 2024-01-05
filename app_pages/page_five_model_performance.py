64447ba2f477b24d16a266a42da0893f0d517de0
KieMerr17 <kieran.merrett@hotmail.com>
2024-01-05 20:05:18 +0000

    add loss/val_loss image to Model Performance page

--- app_pages/page_five_model_performance.py
+++ app_pages/page_five_model_performance.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-import pandas as pd
 from matplotlib.image import imread
-from src.machine_learning.evaluate_clf import load_test_evaluation
 
 
 def page_five_content():
@@ -33,17 +31,34 @@ def page_five_content():
     st.write("### Model Performance ###")
 
     st.write("##### Accuracy and Val_Accuracy #####")
+
     st.warning(
         "Accuracy serves as a metric to assess the overall correctness of the model's predictions against "
         "the true labels in the dataset. It is calculated as the ratio of correct predictions to the total number of predictions made by the model.\n\n"
+        
         "During the model training process, a variant of accuracy known as validation accuracy (val_accuracy) is often used. "
         "In machine learning, it is customary to divide the dataset into training and validation sets. \n\n"
+
         "The training set is employed for model training, while the validation set is used to evaluate the model's "
         "performance throughout the training phase."
     )
-
     model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
     st.image(model_acc, caption='Model Training Accuracy')
+
+
+    st.write("##### Loss and Val_Loss #####")
+
+    st.warning(
+        "Loss is a metric assessing the model's performance throughout the training process. "
+        "This metric measures the difference between the model's predicted "
+        "outputs and the actual true labels attached to the training data.\n\n"
+
+        "Validation loss (val_loss) is a metric used to assess the model's performance on "
+        "an independent validation dataset during the training process. "
+        "It gauges the distinction between the model's predictions and the true labels within the validation set."
+    )
+    model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+    st.image(model_loss, caption='Model Training Losses')
     st.write("---")
 
     
