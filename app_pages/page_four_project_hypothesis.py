96b5476ad94d60098fb1453a882a6a295f93f50b
KieMerr17 <kieran.merrett@hotmail.com>
2024-01-05 19:31:12 +0000

    add hypothesis information and conclusions to page four

--- app_pages/page_four_project_hypothesis.py
+++ app_pages/page_four_project_hypothesis.py
@@ -4,4 +4,30 @@ import streamlit as st
 def page_four_content():
     st.write("### Hypothesis and Conclusion")
 
+    # Hypothesis 1
+    st.info(
+        "Hypothesis 1:\n\n"
+        "It is said that tired drivers may show a higher degree of closed eyes compared to alert drivers. \n\n"
+    )
+
+    st.success(
+        "Conclusion:\n\n"
+        "To validate this hypothesis, we conducted an analysis of the dataset, specifically focusing on the average "
+        "degree of eye openness and closedness.\n\n "
+        "Using Machine Learning, it was shown that the average degree of eye openness in fatigued drivers is notably "
+        "lower than that observed in alert individuals. This observation is reinforced by the examination of both "
+        "Average Image and Variability Images, highlighting the distinction between eyes open and eyes closed."
+    )
+
+    # Hypothesis 2
+    st.info(
+        "Hypothesis 2:\n\n"
+        "It is said that a machine learning model can discern a visual pattern to classify closed and open eyes with an accuracy surpassing 90%."
+    )
+
+    st.success(
+        "Conclusion:\n\n"
+        "To validate this hypothesis, a machine learning model was trained on the dataset, and its performance was evaluated. "
+        "The model achieved an accuracy exceeding 90% on the test set, thereby substantiating the hypothesis."
+    )
 
