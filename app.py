063682d683a91df7d635c937d7f301581d319537
KieMerr17 <kieran.merrett@hotmail.com>
2024-01-05 19:34:37 +0000

    add app/page five model performance, and connect to multipage

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from app_pages.page_one_summary import page_one_content
 from app_pages.page_two_data_visualisation import page_two_content
 from app_pages.page_three_awareness_detector import page_three_content
 from app_pages.page_four_project_hypothesis import page_four_content
+from app_pages.page_five_model_performance import page_five_content
+
 
 
 
@@ -17,6 +19,8 @@ app.add_page("Project Summary", page_one_content)
 app.add_page("Data Visualisation", page_two_content)
 app.add_page("Awareness Detector", page_three_content)
 app.add_page("The Project Hypothesis", page_four_content)
+app.add_page("Model Performance", page_five_content)
+
 
 
 
---
+++ app_pages/page_five_model_performance.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def page_five_content():
+    
+    st.write("### Model Performance ###")
